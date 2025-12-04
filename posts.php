f7ecef8b12a044a7709367cb814963e04b07b3dd
mascahyo15 <mascahyo15@gmail.com>
2025-12-04 16:43:45 +0700

    feat: enable default date filter on posts page

--- posts.php
+++ posts.php
@@ -212,7 +212,7 @@
         };
 
         document.addEventListener('DOMContentLoaded', function() {
-            // setDefaultDateFilter(); // Disabled untuk debug
+            setDefaultDateFilter();
             loadCategories();
             loadPosts();
             subscribeToPosts();
